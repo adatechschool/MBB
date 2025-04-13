07a01fbe24734a38d2458762d938f6ce6bda6d49
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-13 14:49:57 +0200

    docs: ✏️  document code

--- authentication/service/models.py
+++ authentication/service/models.py
@@ -61,10 +61,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     for email, username, active status, and staff status.
     """
 
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    email: models.EmailField = models.EmailField(unique=True)
+    username: models.CharField = models.CharField(max_length=255, unique=True)
+    is_active: models.BooleanField = models.BooleanField(default=True)
+    is_staff: models.BooleanField = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 

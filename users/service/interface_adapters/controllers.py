13af8dd1d084f497ee1ab940527febce2d2d42d0
Ange230700 <kouakouangeericstephane@gmail.com>
2025-04-23 13:25:40 +0200

    test: 💍  do stuff

--- users/service/interface_adapters/controllers.py
+++ users/service/interface_adapters/controllers.py
@@ -3,7 +3,6 @@
 """Controller for user-related HTTP requests and responses."""
 
 from rest_framework import status
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
